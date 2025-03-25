2a086c3205712ce3a5d0dee648f3d6145dfc24cc
jerrit151 <jerrit.schnaible@web.de>
2025-03-25 11:08:03 +0100

    Bodenfeuchtesensor hinzugefügt
    Sensor für die Bodenfeuchtigkeit ins Programm hinzugefügt, Kommentare angepasst

--- Programme/1_Hauptprogramm_BloomBuddy.py
+++ Programme/1_Hauptprogramm_BloomBuddy.py
@@ -1,15 +1,48 @@
+'''
+#-----------------------#
+Programm: 		Automatische Pflanzenbewässerung
+Version: 		V1.0
+
+Programmierer: 	Schnaible
+Datum:			25.03.2025
+
+Hardware:       ESP32 S3
+Sensor: 		Capacitive Soil Moisture Sensor V2.0.0, AHT21, BH1750, VL53L0
+#-----------------------#
+'''
+
 import machine
+from machine import Pin, ADC, SoftI2C
 import time
 import network
 from simple import MQTTClient
-from machine import SoftI2C
 import VL53L0X
 from bh1750 import BH1750
 from aht import AHT21
 import json
 
-# I2C-Bus konfigurieren (gemeinsam für beide Sensoren)
+# I2C-Bus konfigurieren (gemeinsam für alle drei I2C-fähigen-Sensoren)
 i2c = SoftI2C(scl=machine.Pin(12), sda=machine.Pin(13))
+
+#Ausgangs-Pin für das Relais definieren
+relais_in1 = Pin(8, Pin.OUT)
+
+# ADC-Pin für den Sensor
+bodenfeuchte_sensor = Pin(5)
+boden_adc = ADC(bodenfeuchte_sensor)
+
+# ADC-Einstellungen
+boden_adc.atten(ADC.ATTN_11DB)  # Vollbereich: 3.3V
+boden_adc.width(ADC.WIDTH_12BIT)  # Bereich: 0 bis 4095
+
+# Kalibrierwerte
+trocken_wert = 0  # Trockener Boden
+nass_wert = 100  # Nasser Boden
+
+# ADC-Werte für trockenen und nassen Boden (müssen experimentell ermittelt werden)
+# Hier sind Platzhalter, ersetzen Sie diese durch die tatsächlichen ADC-Werte
+trocken_adc_wert = 3070  # Beispielwert für trockenen Boden, reagiert sehr empfindlich auf kleine Änderungen mit diesem Wert in der Luft ca. 2%
+nass_adc_wert = 1700  # Beispielwert für nassen Boden
 
 # Sensoren initialisieren
 aht21_sensor = AHT21(i2c)
@@ -17,11 +50,11 @@ tof_sensor = VL53L0X.VL53L0X(i2c)
 bh1750_sensor = BH1750(i2c)
 
 # WLAN-Parameter
-ssid = 'KrustyKrab2'
-password = 'WerderBremen2501'
+ssid = 'BZTG-IoT'
+password = 'WerderBremen24'
 
 # MQTT-Setup
-BROKER_IP = b"192.168.33.79"
+BROKER_IP = b"192.168.1.211"
 BROKER_PORT = 1883
 CLIENT_ID = b"ESP32_Client"
 TOPIC = b"Zuhause/Wohnung/BloomBuddy"
@@ -53,6 +86,27 @@ except Exception as e:
         pass
 
 while True:
+    
+    # Sensorwert lesen
+    boden_adc_wert = boden_adc.read()
+        
+    # Berechnung des Feuchtigkeitswertes basierend auf den Kalibrierwerten
+    bodenfeuchtigkeit = ((boden_adc_wert - nass_adc_wert) / (trocken_adc_wert - nass_adc_wert)) * (trocken_wert - nass_wert) + nass_wert
+        
+    # Sicherstellen, dass der Wert im Bereich von 0 bis 100 bleibt
+    bodenfeuchtigkeit = max(0, min(bodenfeuchtigkeit, 100))
+    # Wert der Bodenfeuchtigkeit ausgeben    
+    print(f'Bodenfeuchtigkeit: {bodenfeuchtigkeit:.1f}%')
+        
+    # Fällt die Bodenfeuchtigkeit unter 20%, soll das Relais der Pumpe angesteuert werden
+    if bodenfeuchtigkeit <= 20:
+        relais_in1.value(1)  # Ausgang auf TRUE setzen
+        print("Relais angesteuert, Pumpe ein")
+    else:
+        relais_in1.value(0)  # Ist die Bodenfeuchtigkeit nicht unter 20%, bleibt das Relais und die Pumpe aus
+        print("Relais nicht angesteuert, Pumpe aus")
+
+   
     # Listen zum speichern der Messwerte
     fuellstand_roh = []
     temperatur_roh = []
@@ -109,7 +163,8 @@ while True:
         "Fuellstand": fuellstand,
         "Temperatur": temperatur,
         "Luftfeuchtigkeit": feuchtigkeit,
-        "Helligkeit": helligkeit
+        "Helligkeit": helligkeit,
+        "Bodenfeuchtigkeit": bodenfeuchtigkeit
     }
     
     # JSON-Objekt in eine Zeichenkette umwandeln
@@ -123,12 +178,14 @@ while True:
     temperatur_roh.clear()
     feuchtigkeit_roh.clear()
     helligkeit_roh.clear()
+    
     # Prüfen, ob die Liste wirklich leer ist
     print("Liste nach dem Leeren:", fuellstand_roh)
     print("Liste nach dem Leeren:", temperatur_roh)
     print("Liste nach dem Leeren:", feuchtigkeit_roh)
     print("Liste nach dem Leeren:", helligkeit_roh)
+    
     # Rückmeldung ob der Wert gesendet wurde
     print(f"Sensordaten gesendet: {json_data}")
     
-    time.sleep(20)
+    time.sleep(45)
